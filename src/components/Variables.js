cd04f88e5846c755d72ef9b71e92f9e8312e72e0
Salman_JS <salman4javascript@gmail.com>
2022-10-13 03:02:02 +0530

    BUILD-003

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -1,7 +1,9 @@
 const Variables = {
-    hostId : "https://63432a99c9538913499b7457--guileless-sundae-522a29.netlify.app/.netlify/functions/server"
+    hostId : "https://guileless-sundae-522a29.netlify.app/.netlify/functions/server"
 }
 
 //http://localhost:9000/.netlify/functions/server/
+//https://63432a99c9538913499b7457--guileless-sundae-522a29.netlify.app/.netlify/functions/server
+
 
 export default Variables;
