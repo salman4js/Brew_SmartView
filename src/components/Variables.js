22e2c55572e30ebd0f0583b5392e0b029f0beffa
Salman_JS <salman4javascript@gmail.com>
2022-10-13 20:31:06 +0530

    Dish Type and Order dishes for the room has been fixed.

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -1,9 +1,10 @@
 const Variables = {
-    hostId : "https://guileless-sundae-522a29.netlify.app/.netlify/functions/server"
+    hostId : "https://salman-dev--guileless-sundae-522a29.netlify.app/.netlify/functions/server"
 }
 
 //http://localhost:9000/.netlify/functions/server/
 //https://63432a99c9538913499b7457--guileless-sundae-522a29.netlify.app/.netlify/functions/server
+//https://guileless-sundae-522a29.netlify.app/.netlify/functions/server
 
 
 export default Variables;
