931c3e31b648c1ac62e2e96bcc7faf135ee33be3
Salman_JS <salman4javascript@gmail.com>
2023-03-08 12:27:49 +0530

    Fix the glitches on old version and fixed the dates issue on the dashboard

---
+++ src/components/Loader/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Spinner = () => {
+    return (
+        <div className="text-center">
+            <div className="spinner">
+
+            </div>
+        </div>
+    )
+}
+
+export default Spinner
