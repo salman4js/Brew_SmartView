ae8fb134be87513de4bc427d5700f48f2347b39b
salman4js <salman4javascript@gmail.com>
2023-06-24 00:21:18 +0530

    Added user logout in vouchers

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,5 +1,8 @@
+const storage = require("../Controller/Storage/Storage")
+
 const changeScreen = () => {
     console.log("Changing screen, cause token has expired");
+    storage.clearStorage(); // Clearing out the local storage on every session expires!
     window.location.href = "/login";
 }
 
