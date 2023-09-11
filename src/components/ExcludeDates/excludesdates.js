bfb7025e99a1f1a2f1d00b8a83bd94cc471aa7ad
salman4js <salman4javascript@gmail.com>
2023-09-11 20:55:57 +0530

    Enabled loader when shifting room from the sidepanel so that the user are aware that the state is changing

--- src/components/ExcludeDates/excludesdates.js
+++ src/components/ExcludeDates/excludesdates.js
@@ -4,10 +4,10 @@ var Variables = require('../Variables').default;
 export const excludeDatesCheckin = async (props) => {
     const res = await axios.get(`${Variables.hostId}/${props}/excludeDatescheckin`);
     return res.data;
-}
+};
 
 // Pre book exclude dates!
 export const prebookExcludeDates = async (props) => {
     const res = await axios.get(`${Variables.hostId}/${props}/excludedates`);
     return res.data;
-}
+};
