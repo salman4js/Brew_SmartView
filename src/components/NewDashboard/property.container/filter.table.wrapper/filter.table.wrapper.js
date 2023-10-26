49cc36b10b26ca41eea92123580bfa0d300c4fbe
salman4js <salman4javascript@gmail.com>
2023-10-27 03:52:33 +0530

    Added date filter in filterTableView

--- src/components/NewDashboard/property.container/filter.table.wrapper/filter.table.wrapper.js
+++ src/components/NewDashboard/property.container/filter.table.wrapper/filter.table.wrapper.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import _ from 'lodash';
+import brewDate from 'brew-date';
 import TableView from '../table.view/table.view';
 import CollectionInstance from '../../../../global.collection/widgettile.collection/widgettile.collection';
 
@@ -35,8 +36,16 @@ class FilterTable extends TableView {
   _applyFilter(){
     var filterData = this.state.data.filteredData,
       self = this;
-    _.remove(this.filteredModel[this.props.data.selectedRoomConstant], function(obj){ // this handle filter by room type!
+    // this handle filter by room type!
+    _.remove(this.filteredModel[this.props.data.selectedRoomConstant], function(obj){
         return obj.suiteName !== filterData.suiteType;
+    });
+    // this handle dateofcheckout filter!
+    _.remove(this.filteredModel[this.props.data.selectedRoomConstant], function(obj){
+      if(obj.prebookDateofCheckin.length > 0){
+        var inBetweenDates = brewDate.getBetween(brewDate.getFullDate('yyyy/mm/dd'), obj.prebookDateofCheckin);
+        return !inBetweenDates.includes(filterData.checkinDate); 
+      };
     });
   };
   
@@ -46,9 +55,24 @@ class FilterTable extends TableView {
     this.templateHelpersData.selectedRoomConstant = userStatusMap[this.props.data.selectedRoomConstant];
   };
   
+  // Prepare table state for filterTable!
+  prepareTableState(){
+    this.metadataTableState.enableCheckbox = true;
+    this.metadataTableState.checkbox = [
+      {
+        select: null,
+        value: false,
+        attribute: "checkBoxField",
+        enableCellCheckbox: true,
+        enableHeaderCheckbox: false
+      }
+    ]
+  };
+  
   // Get the filtered data based on the filter applied by the user!
   getFilteredData(){
     this.getTableHeaderValue();
+    this.prepareTableState();
     this.roomConstant = this.props.data.selectedRoomConstant;
     this.filterRoomCollection();
   };
