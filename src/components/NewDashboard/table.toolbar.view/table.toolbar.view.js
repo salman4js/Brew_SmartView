2a2dad987526743f7345bc148ba1c2c47cbb4597
salman4js <salman4javascript@gmail.com>
2023-11-29 22:26:23 +0530

    Added checkbox selection in base table view

---
+++ src/components/NewDashboard/table.toolbar.view/table.toolbar.view.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class TableToolbarView extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    templateHelpers(){
+
+    };
+
+    render(){
+      return this.templateHelpers();
+    };
+}
+
+export default TableToolbarView;
